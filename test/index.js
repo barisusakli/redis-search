b12a2b8df9b61c9a97ac609806117ae5d517ecb3
Barış Soner Uşaklı <barisusakli@gmail.com>
2021-05-11 12:48:35 -0400

    feat: rewrite to use ioredis
    remove async dependency use async/await
    fix tests

--- test/index.js
+++ test/index.js
@@ -3,142 +3,101 @@
 
 /* globals before, after, describe, it */
 
-var redisSearch = require('../index');
-var async = require('async');
-var assert = require('assert');
+const redisSearch = require('../index');
+const assert = require('assert');
 
-var postSearch = redisSearch.createSearch('mypostsearch');
+const postSearch = redisSearch.createSearch('mypostsearch');
 
-before(function(done) {
-  async.parallel([
-    function(next) {
-      postSearch.index({content: 'ruby emerald', uid: 5, cid: 1}, 1, next);
-    },
-    function(next) {
-      postSearch.index({content: 'emerald orange emerald', uid: 5, cid: 2}, 2, next);
-    },
-    function(next) {
-      postSearch.index({content: 'cucumber apple orange', uid: 4, cid: 2}, 3, next);
-    },
-    function(next) {
-      postSearch.index({content: 'ORANGE apple pear', uid: 5, cid: 4}, 4, next);
-    },
-    function(next) {
-      postSearch.index({content: 'dog cat', uid: 6, cid: 4}, 5, next);
-    }
-  ], done);
+before(async function() {
+  await Promise.all([
+    postSearch.index({content: 'ruby emerald', uid: 5, cid: 1}, 1),
+    postSearch.index({content: 'emerald orange emerald', uid: 5, cid: 2}, 2),
+    postSearch.index({content: 'cucumber apple orange', uid: 4, cid: 2}, 3),
+    postSearch.index({content: 'ORANGE apple pear', uid: 5, cid: 4}, 4),
+    postSearch.index({content: 'dog cat', uid: 6, cid: 4}, 5),
+  ]);
 });
 
 
 describe('query', function() {
-  it('should find the correct ids', function(done) {
-    postSearch.query({content: 'apple orange', uid: 5}, function(err, ids) {
-      assert.ifError(err);
-      assert.equal(ids.length, 2);
-      assert.notEqual(ids.indexOf('2'), -1);
-      assert.notEqual(ids.indexOf('4'), -1);
-      done();
+  it('should find the correct ids', async function() {
+    const ids = await postSearch.query({
+      matchWords: 'any',
+      query: { content: 'apple orange', uid: 5 },
     });
+
+    assert.strictEqual(ids.length, 2);
+    assert(ids.includes('2'));
+    assert(ids.includes('4'));
   });
 
-  it('should return empty', function(done) {
-    postSearch.query({uid: 5, content: 'apple orange', cid: 123}, function(err, ids) {
-      assert.ifError(err);
-      assert.equal(ids.length, 0);
-      done();
-    });
+  it('should return empty', async function () {
+    const ids = await postSearch.query({ query: {uid: 5, content: 'apple orange', cid: 123 } });
+    assert.strictEqual(ids.length, 0);
   });
 
-  it('should return 1', function(done) {
-    postSearch.query({uid: 5, content: 'emerald', cid: 1}, function(err, ids) {
-      assert.ifError(err);
-      assert.equal(ids.length, 1);
-      assert.equal(ids[0], 1);
-      done();
-    });
+  it('should return 1', async function () {
+    const ids = await postSearch.query({ query: { uid: 5, content: 'emerald', cid: 1 } } );
+    assert.strictEqual(ids.length, 1);
+    assert.strictEqual(ids[0], '1');
   });
 
-  it('should return all the ids with uid=5', function(done) {
-    postSearch.query({uid: 5}, function(err, ids) {
-      assert.ifError(err);
-      assert.equal(ids.length, 3);
-      assert.notEqual(ids.indexOf('1'), -1);
-      assert.notEqual(ids.indexOf('2'), -1);
-      assert.notEqual(ids.indexOf('4'), -1);
-      done();
-    });
+  it('should return all the ids with uid=5', async function () {
+    const ids = await postSearch.query({ query: { uid: 5 }});
+    assert.strictEqual(ids.length, 3);
+    assert(ids.includes('1'));
+    assert(ids.includes('2'));
+    assert(ids.includes('4'));
   });
 
-  it('should return return 4', function(done) {
-    postSearch.query({content: 'ruby pear', cid: 4}, function(err, ids) {
-      assert.ifError(err);
-      assert.equal(ids.length, 1);
-      assert.notEqual(ids.indexOf('4'), -1);
-      done();
+  it('should only return 4', async function () {
+    const ids = await postSearch.query({
+      matchWords: 'any',
+      query: { content: 'ruby pear', cid: 4 }
     });
+    assert.strictEqual(ids.length, 1);
+    assert(ids.includes('4'));
   });
 
-  it('should limit the results to 2', function(done) {
-    postSearch.query({uid: 5}, 0, 1, function(err, ids) {
-      assert.ifError(err);
-      assert.equal(ids.length, 2);
-      done();
-    });
+  it('should limit the results to 2', async function () {
+    const ids = await postSearch.query({ query: { uid: 5 } }, 0, 1);
+    assert.strictEqual(ids.length, 2);
   });
 
-  it('should return the correct ids when an array is passed in', function(done) {
-    postSearch.query({cid: [2, 4]}, function(err, ids) {
-      assert.ifError(err);
-      assert.equal(ids.length, 4);
-      assert.notEqual(ids.indexOf('2'), -1);
-      assert.notEqual(ids.indexOf('3'), -1);
-      assert.notEqual(ids.indexOf('4'), -1);
-      assert.notEqual(ids.indexOf('5'), -1);
-      done();
-    });
+  it('should return the correct ids when an array is passed in', async function () {
+    const ids = await postSearch.query({ query: { cid: [2, 4] } });
+    assert.strictEqual(ids.length, 4);
+    assert(ids.includes('2'));
+    assert(ids.includes('3'));
+    assert(ids.includes('4'));
+    assert(ids.includes('5'));
   });
 
-  it('should return the correct ids when an array is passed in', function(done) {
-    postSearch.query({cid: [2, 4], content: 'orange', uid: 4}, function(err, ids) {
-      assert.ifError(err);
-      assert.equal(ids.length, 1);
-      assert.notEqual(ids.indexOf('3'), -1);
-      done();
+  it('should return the correct ids when an array is passed in', async function () {
+    const ids = await postSearch.query({
+      query: {
+        cid: [2, 4], content: 'orange', uid: 4
+      }
     });
+
+    assert.strictEqual(ids.length, 1);
+    assert(ids.includes('3'));
   });
 
-  it('should find the match when query is upper case', function(done) {
-    postSearch.query({content: 'CUCUMBER'}, function(err, ids) {
-      assert.ifError(err);
-      assert.equal(ids.length, 1);
-      assert.notEqual(ids.indexOf('3'), -1);
-      done();
-    });
-  })
-
+  it('should find the match when query is upper case', async function () {
+    const ids = await postSearch.query({ query: { content: 'CUCUMBER' } })
+    assert.strictEqual(ids.length, 1);
+    assert(ids.includes('3'));
+  });
 });
 
 
-after(function(done) {
-  async.parallel([
-    function(next) {
-      postSearch.remove(1, next);
-    },
-    function(next) {
-      postSearch.remove(2, next);
-    },
-    function(next) {
-      postSearch.remove(3, next);
-    },
-    function(next) {
-      postSearch.remove(4, next);
-    },
-    function(next) {
-      postSearch.remove(5, next);
-    }
-  ], done);
+after(async function () {
+  await Promise.all([
+    postSearch.remove(1),
+    postSearch.remove(2),
+    postSearch.remove(3),
+    postSearch.remove(4),
+    postSearch.remove(5),
+  ]);
 });
-
-
-
-
